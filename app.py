8744d0fcdcc2eb9deaafcab4a28e9617831e9eeb
Silla Shaju <saranammanariyil@gmail.com>
2025-07-28 13:11:18 +0530

    Initial commit with project files and images

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import joblib
+import threading
+import webbrowser
+import time
+
+app = Flask(__name__)
+
+# Load model and vectorizer
+model = joblib.load('sentiment_model.pkl')
+vectorizer = joblib.load('vectorizer.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        comment = request.form['comment']
+        data = vectorizer.transform([comment])
+        prediction = model.predict(data)[0]
+        return render_template('index.html', comment=comment, prediction=prediction)
+
+def open_browser():
+    # Give Flask a second to start
+    time.sleep(1)
+    webbrowser.open_new('http://127.0.0.1:5000/')
+
+if __name__ == '__main__':
+    # Start browser thread
+    threading.Thread(target=open_browser).start()
+    
+    # Start Flask app
+    app.run(debug=False, use_reloader=False)
